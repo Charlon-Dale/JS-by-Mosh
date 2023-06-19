199a17eca8a28851182b90725452f8760a363382
Charlon-Dale <charlondalesanagugstin@gmail.com>
2023-06-19 12:19:22 +0800

    used primitive types

--- index.js
+++ index.js
@@ -5,4 +5,14 @@
  let lastName = 'Austin';
 
  console.log(firstName);
- console.log(lastName);
+ console.log(lastName); 
+
+ const interestRate = 0.3;
+
+ console.log(interestRate);
+
+ let name = 'Mosh';
+ let age = 30;
+ let isApproved = false;
+ let noName = undefined;
+ let selectedColor = null;
