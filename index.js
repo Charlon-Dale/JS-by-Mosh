27297e8b4f83055de84916dd942473754263e0f6
Charlon-Dale <charlondalesanagugstin@gmail.com>
2023-06-19 12:24:53 +0800

    separate topics into each js files

--- index.js
+++ index.js
@@ -6,13 +6,3 @@
 
  console.log(firstName);
  console.log(lastName); 
-
- const interestRate = 0.3;
-
- console.log(interestRate);
-
- let name = 'Mosh';
- let age = 30;
- let isApproved = false;
- let noName = undefined;
- let selectedColor = null;
---
+++ primitive.js
@@ -0,0 +1,11 @@
+let names = 'Mosh';
+let age = 30;
+let isApproved = false;
+let noName = undefined;
+let selectedColor = null;
+
+console.log(names);
+console.log(age);
+console.log(isApproved);
+console.log(noName);
+console.log(selectedColor);
